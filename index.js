af8ae3f638e8f42bd5bfaa2709c088eb861b4d6c
Alexisson <ivan.a.shirokov@ya.ru>
2021-09-24 20:06:12 +0300

    docker-nodejs-demoapp v1.2

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import os from "os";
 import exphbs from "express-handlebars";
 import Handlebars from "handlebars";
 import { allowInsecurePrototypeAccess } from "@handlebars/allow-prototype-access";
+import fs from "fs";
 
 const PORT = 80;
-const host = "0.0.0.0";
-
+var index = 0;
 const app = express();
 const hbs = exphbs.create({
   defaultLayout: "main",
@@ -17,12 +17,23 @@ app
   .engine("hbs", hbs.engine)
   .set("view engine", "hbs")
   .set("views", "views")
-  .all("/", (r) =>
+  .all("/", (r) => {
+    let data = fs.readFileSync("index.json");
+    r.res.render("index", { index: JSON.parse(data).index });
+  })
+  .all("/stat", (r) => {
+    let data = fs.readFileSync("index.json");
+    let index = JSON.parse(data).index + 1;
+    fs.writeFileSync("./index.json", JSON.stringify({ index }));
+    r.res.render("index", { index });
+  })
+  .all("/about", (r) =>
     r.res.render("home", {
       port: PORT,
-      hostname: host,
+      hostname: "localhost",
       title: "Стартовая страница",
       name: "Иван",
     })
   )
-  .listen(PORT, () => console.log(`Running on http://${host}:${PORT}`));
+
+  .listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
